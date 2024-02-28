75137d0da4f10c4673a20a1cefe7e08390ce5e4d
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-02-28 19:38:58 -0300

    inicinado o projeto..

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        'darkMode-bg': 'hsl(235, 21%, 11%)',
+        'darkMode-container': 'hsl(235, 24%, 19%)',
+        'gradientColor-btn-from': 'hsl(192, 100%, 67%)',
+        'gradientColor-btn-to': ' hsl(280, 87%, 65%)'
+      },
+      width: {
+        '500': '500px',
+      }
+    },
+  },
+  plugins: [],
+}
